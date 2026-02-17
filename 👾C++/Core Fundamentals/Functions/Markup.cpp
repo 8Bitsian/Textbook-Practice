e5e2f50a422ead6a784f2f13d24350c2705a3ee3
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-02-16 19:49:30 -0500

    Update Markup.cpp

--- 👾C++/Core Fundamentals/Functions/Markup.cpp
+++ 👾C++/Core Fundamentals/Functions/Markup.cpp
@@ -3,34 +3,58 @@
   Programming Exercise: Markup
   Design a program that asks the user to enter an item's wholesale cost and its markup percentage.
   It should then display the item's retail price.
-  Example:
+  Examples:
   - If an item's wholesale cost is 5.00 and its markup percentage is 100%, then the item's retail price is 10.00
   - If an item's wholesale cost is 5.00 and its markup percentage is 50%, then the item's retail price is 7.50
-  
   The program should have a function named calculateRetail that recieves the wholesale cost and the
   markup percentage as arguments and returns the retail price of the item.
   Input validation: Do not accept negative values for either the wholesale cost nor the markup percentage.
 */
 
-# Get user input
-print("What is your name?")
-firstName = input('First Name? ')
-lastName = input('Last Name? ')
+#include <iostream>
+#include <iomanip>
+#include <string>
 
-print("What is your address?")
-street = input('Street? ')
-city = input('City? ')
-state = input('State? ')
-ZIP = int(input('ZIP? '))
+using namespace std;
 
-print("What is your phone number?")
-number = int(input('Phone? '))
+// Function Prototypes
+double calculateRetail(double price, double markup);
 
-print("What is your college major?")
-major = input('Major? ')
+// main()
+int main() {
+  // Declare variables
+  double wholesaleCost, markupPercentage, retailPrice;
 
-# Output personal information
-print("My name is", firstName, lastName)
-print("I live at", street, city, state, ZIP)
-print("My number is", number)
-print("My major is", major)
+  // Get user input and validate for wholesale cost
+  do {
+    cout << "Enter the item's wholesale cost: $";
+    cin >> wholesaleCost;
+    if (wholesaleCost < 0)
+      cout << "ERROR: Invalid Input.\n";
+  } while (wholesaleCost < 0);
+  
+  // Get user input and validate for markup percentage
+  do {
+    cout << "Enter the item's markup percentage: ";
+    cin >> markupPercentage;
+    if (markupPercentage < 0)
+      cout << "ERROR: Invalid Input.\n";
+  } while (markupPercentage < 0);
+
+  // Call functions
+  retailPrice = calculateRetail(wholesaleCost, markupPercentage);
+
+  // Output
+  cout << fixed << setprecision(2);
+  cout << "\nPrice Adjustment\n";
+  cout << "Wholesale Cost: $" << wholesaleCost << endl;
+  cout << "Markup Percentage: " << markupPercentage << "%" << endl;
+  cout << "Retail Price: $" << retailPrice;
+
+  return 0;
+}
+
+// calculateRetail()
+double calculateRetail(double wholesale, double markup) {
+  return wholesale * (1 + markup / 100);
+}
